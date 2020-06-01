2bfeb9b27832f2b2f8d105c5641dcea482bfe178
sunnyspeedstudio <seesunmoon@gmail.com>
2020-06-01 14:19:30 -0700

    add couple more features

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # GitHub: https://github.com/sunnyspeedstudio/favorite-focal-length
 # YouTube: https://www.youtube.com/sunnyspeedstudio
 # Bili Bili: http://space.bilibili.com/36609936
+# Tested using python 3
 
 
 import subprocess
@@ -9,9 +10,12 @@ import os
 import operator
 
 # all image folders need to be scanned (the script will go through all subfolders)
-imgFolders = ['full path to the image folder', 'another full path to the image folder']
+imgFolders = ['full path to the image folder', 'full path to another image folder']
 # all image extensions need to be scanned
 imgExtensions = ('.jpg', '.JPG', '.DNG', '.CR2', '.NEF', '.ARW')
+# exiftool path
+exiftoolPath = 'exiftool'
+
 
 totalImg = 0
 focalLength = {}
@@ -22,7 +26,7 @@ for imgFolder in imgFolders:
         for f in files:
             imgFile = subfolder + os.sep + f
             if imgFile.endswith(imgExtensions):
-                process = subprocess.Popen(['exiftool', imgFile], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True) 
+                process = subprocess.Popen([exiftoolPath, imgFile], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True) 
                 exifInfo = {}
                 exifInfo['Focal Length'] = 'Unknown'
                 exifInfo['Lens ID'] = 'Unknown'
@@ -33,6 +37,10 @@ for imgFolder in imgFolders:
                     exifInfo[kv[0].strip()] = kv[1].strip()
 
                 process.kill()
+
+                # remove '35 mm equivalent'
+                kv = exifInfo['Focal Length'].split(' (', 1)
+                exifInfo['Focal Length'] = kv[0]
 
                 if exifInfo['Focal Length'] in focalLength.keys():
                     focalLength[exifInfo['Focal Length']] += 1
@@ -45,17 +53,17 @@ for imgFolder in imgFolders:
                     lensID[exifInfo['Lens ID']] = 1
                 
                 totalImg += 1
-                print totalImg, ':', imgFile, ':', exifInfo['Focal Length'], ':', exifInfo['Lens ID']
+                print(totalImg, ':', imgFile, ':', exifInfo['Focal Length'], ':', exifInfo['Lens ID'])
 
 # print out results
-print '\nTotal Images Scanned: ', totalImg
+print('\nTotal Images Scanned: ', totalImg)
 
-print '\nFocal Length:'
+print('\nFocal Length:')
 sorted_focalLength = sorted(focalLength.items(), key=operator.itemgetter(1), reverse=True)
 for x in sorted_focalLength:
-    print x[0] + ' = ', x[1]
+    print(x[0], '=', x[1], '-', "{:.2f}".format(x[1] * 100 / totalImg), '%')
 
-print '\nLens:'
+print('\nLens:')
 sorted_lensID = sorted(lensID.items(), key=operator.itemgetter(1), reverse=True)
 for x in sorted_lensID:
-    print x[0] + ' = ', x[1]
+    print(x[0], '=', x[1], '-', "{:.2f}".format(x[1] * 100 / totalImg), '%')
